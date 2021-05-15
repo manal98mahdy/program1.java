365b8ed2c219db57517f7f5e612e3df1ad5b2acc
manal98mahdy <83515082+manal98mahdy@users.noreply.github.com>
2021-05-15 23:08:20 +0200

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class StringsFileIoCollections {
+    public static void main(String[] args) {
+
+        System.out.println("program 1 : Strings");
+        System.out.println("input your name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        String upper = name.toUpperCase();
+        char c = upper.charAt(0);
+        System.out.print(c);
+        int len = upper.length();
+        for (int i = 1; i < len; i++) {
+            char c2 = upper.charAt(i);
+            if (c2 == ' ') {
+                System.out.print(" . " + upper.charAt(i + 1));
+            }
+        }
+        
+    }}
